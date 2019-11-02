1ed4c35c0e42ffa7024914e55ab316be217378c6
Florian Kempenich <Flori@nKempenich.com>
2019-11-02 15:19:37 +0000

    Display all list items on Homepage

--- lists/tests.py
+++ lists/tests.py
@@ -27,6 +27,16 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        content = response.content.decode()
+        self.assertIn('itemey 1', content)
+        self.assertIn('itemey 2', content)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- notes.md
+++ notes.md
@@ -2,6 +2,6 @@
 
 [x] Do not save blank items for every request
 [x] Code smell: POST test is too long?
-[ ] Display multiple items in the table
+[x] Display multiple items in the table
 [ ] Support more than one list
 [ ] Allow multiple lists for multiple users
