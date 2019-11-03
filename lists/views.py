cabc5cedfef8ff24e4939689b8955ef4749c434b
Florian Kempenich <Flori@nKempenich.com>
2019-11-03 13:02:15 +0000

    Use separate template for ListView

--- lists/views.py
+++ lists/views.py
@@ -10,7 +10,7 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
 
-    return render(request, 'home.html', {'items': Item.objects.all()})
+    return render(request, 'home.html')
 
 def view_list(request):
-    return render(request, 'home.html', {'items': Item.objects.all()})
+    return render(request, 'list.html', {'items': Item.objects.all()})
