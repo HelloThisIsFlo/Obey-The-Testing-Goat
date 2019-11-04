51be1c36909ae9d8879eda4dbd386259e1b2f6a2
Florian Kempenich <Flori@nKempenich.com>
2019-11-04 16:41:54 +0000

    Use envvars for Prod settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,19 +19,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6kjmo85rj-qu!==2&l^y1q4_sr%naiferb*@a8-&kl_bb%ffo#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    # # Not needed for now, and clutters the 
+    # # Not needed for now, and clutters the
     # # static files explanation
     # 'django.contrib.admin',
     'django.contrib.auth',
