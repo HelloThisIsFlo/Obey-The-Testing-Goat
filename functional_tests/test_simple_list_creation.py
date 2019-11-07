16fd091cd9b2face5a23eb9ea4c2a61a16138808
Florian Kempenich <Flori@nKempenich.com>
2019-11-07 11:53:35 +0000

    Refactor FT into separate files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,38 +1,9 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import time
-import os
-import unittest
-from unittest import skip
-
-MAX_WAIT = 3
+from .base import FunctionalTest
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time >= MAX_WAIT:
-                    raise e
-                time.sleep(0.1)
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes to check
         # out its homepage
@@ -116,42 +87,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-    def test_layout_and_styling(self):
-        def ensure_input_box_centered():
-            center_of_input_box = input_box.location['x'] + \
-                input_box.size['width'] / 2
-            self.assertAlmostEqual(center_of_input_box, 512, delta=10)
-
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        input_box = self.browser.find_element_by_id('id_new_item')
-        ensure_input_box_centered()
-
-        # She starts a new list and sees the input is nicely
-        # centered there too
-        input_box.send_keys('testing')
-        input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        input_box = self.browser.find_element_by_id('id_new_item')
-        ensure_input_box_centered()
-
-    @skip
-    def test_cannot_add_empty_list_items(self):
-        # Edith goes to the home page and accidentally tries to submit
-        # an empty list item. She hits Enter on the empty input box
-
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
-
-        # She tries again with some text for the item, which now works
-
-        # Perversely, she now decides to submit a second blank list item
-
-        # She receives a similar warning on the list page
-
-        # And she can correct it by filling some text in
-        self.fail('TODO: Implement!')
