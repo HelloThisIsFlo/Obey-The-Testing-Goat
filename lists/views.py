86f23981081e7e0cbe8d519f50347bf99909fe95
Florian Kempenich <Flori@nKempenich.com>
2019-11-10 17:42:52 +0000

    Use ItemForm in Homepage view

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,11 @@ from django.core.exceptions import ValidationError
 from textwrap import dedent
 
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
