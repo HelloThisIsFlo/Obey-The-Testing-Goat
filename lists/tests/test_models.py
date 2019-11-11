18fdc76d55f825a85f65f5715c51ddbc0f0ece36
Florian Kempenich <Flori@nKempenich.com>
2019-11-11 11:58:56 +0000

    Ensure duplicates items can't be saved to the same list (DB level)

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -38,6 +38,30 @@ class ListAndItemModelTest(TestCase):
         with self.assertRaises(ValidationError):
             item.full_clean()
 
+    def test_cannot_validate_duplicate_list_item(self):
+        duplicate_text = 'some item'
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text=duplicate_text)
+
+        duplicate_item = Item(list=list_, text=duplicate_text)
+        with self.assertRaises(ValidationError):
+            duplicate_item.full_clean()
+
+    def test_can_save_same_item_to_different_lists(self):
+        duplicate_text = 'some item'
+        list_1 = List.objects.create()
+        list_2 = List.objects.create()
+
+        Item.objects.create(list=list_1, text=duplicate_text)
+
+        # When creating an item with the same description,
+        # but in `list_2`
+        # No exception should be raised
+        duplicate_item_in_different_list = Item(
+            list=list_2, text=duplicate_text
+        )
+        duplicate_item_in_different_list.full_clean()
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
