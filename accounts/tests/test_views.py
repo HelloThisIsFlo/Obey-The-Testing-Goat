f735de33bbb92a670d5976ab20e015e2059ec8b5
Florian Kempenich <Flori@nKempenich.com>
2019-11-15 19:34:03 +0000

    Merge my version of the tests with the updated  one from the book

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -20,37 +20,16 @@ class SendLoginEmailViewTest(TestCase):
         self.assertRedirects(response, '/')
 
     @patch('accounts.views.send_mail')
-    def test_sends_mail_to_address_from_post(self, send_mail_mock):
-        self.client.post('/accounts/send_login_email', data={
-            'email': 'edith@example.com'
-        })
-        send_mail_mock.assert_called_with(
-            'Your login link for Superlists',
-            ANY,
-            'noreply@superlists',
-            ['edith@example.com'],
-        )
-
-    def test_sends_mail_to_address_from_post_BOOK(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
-
+    def test_sends_mail_to_address_from_post(self, mock_send_mail):
         self.client.post('/accounts/send_login_email', data={
             'email': 'edith@example.com'
         })
 
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, 'Your login link for Superlists')
-        self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, ['edith@example.com'])
+        mock_send_mail.assert_called_once()
+        (subject, _body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertEqual(subject, 'Your login link for Superlists')
+        self.assertEqual(from_email, 'noreply@superlists')
+        self.assertEqual(to_list, ['edith@example.com'])
 
     def test_sends_mail_to_address_from_post_DJANGO_MOCK(self):
         self.client.post('/accounts/send_login_email', data={
