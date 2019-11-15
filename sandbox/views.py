1a4713373ca82c96d02e0b6dfb74bd33e4a07df7
Florian Kempenich <Flori@nKempenich.com>
2019-11-15 09:43:28 +0000

    Setup basic form

--- sandbox/views.py
+++ sandbox/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def main(request):
+    if request.method == 'POST':
+        data = {'email': request.POST['email']}
+    else:
+        data = {}
+
+    return render(request, 'sandbox/main.html', data)
--- superlists/settings.py
+++ superlists/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Project
     'lists',
+    'sandbox'
 ]
 
 MIDDLEWARE = [
