88fbc7da0bde55c25cf890f81345982a7acf2b8a
Florian Kempenich <Flori@nKempenich.com>
2019-11-15 15:45:58 +0000

    Implement auth

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -4,4 +4,6 @@ from sandbox import views
 
 urlpatterns = [
     url(r'^$', views.main, name='sandbox_main'),
+    url(r'^login$', views.login, name='sandbox_login'),
+    url(r'^logout$', views.logout, name='sandbox_logout'),
 ]
--- sandbox/views.py
+++ sandbox/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import auth
 
 
 def main(request):
-    return HttpResponse('ok')
+    return render(request, 'sandbox/main.html')
+
+
+def login(request):
+    user = auth.authenticate(
+        request,
+        username=request.POST['username'],
+        password=request.POST['pass']
+    )
+    if user is None:
+        print('auth failed')
+    else:
+        auth.login(request, user)
+
+    return redirect('sandbox_main')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('sandbox_main')
