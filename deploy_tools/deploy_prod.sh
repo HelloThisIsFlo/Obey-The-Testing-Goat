57a99c061549a4b2b1662b5539e72b200dddfb76
Florian Kempenich <Flori@nKempenich.com>
2019-11-17 19:25:24 +0000

    Update deploy tools

--- deploy_tools/deploy_prod.sh
+++ deploy_tools/deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-fab -A deploy:host=floriank@goat-staging.ddns.net -f $DIR/fabfile.py
+fab -A deploy:host=floriank@goat-production.ddns.net -f $DIR/fabfile.py
