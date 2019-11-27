03bd94bc815e8fbab3789582c613e861d4d76c02
Florian Kempenich <Flori@nKempenich.com>
2019-11-27 17:22:44 +0000

    Implement basic REST Api

---
+++ lists/api.py
@@ -0,0 +1,26 @@
+import json
+
+from django.http import HttpResponse
+from lists.models import List, Item
+
+
+def list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+
+    if request.method == 'POST':
+        Item.objects.create(list=list_, text=request.POST['text'])
+        return HttpResponse(status=201)
+
+    elif request.method == 'GET':
+        item_dicts = [
+            {'id': item.id, 'text': item.text}
+            for item in list_.item_set.all()
+        ]
+
+        return HttpResponse(
+            json.dumps(item_dicts),
+            content_type='application/json'
+        )
+
+    else:
+        raise NotImplementedError()
