662eb02842f0315c6181def1d43ad08371e2a984
Florian Kempenich <Flori@nKempenich.com>
2019-11-27 17:39:39 +0000

    Implement AJAX logic

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,9 +1,22 @@
 window.Superlists = {};
-window.Superlists.initialize = function() {
+window.Superlists.initialize = function(url) {
   function hideError() {
     $(".has-error").hide();
   }
 
   $('input[name="text"]').on("keypress", hideError);
   $('input[name="text"]').on("click", hideError);
+
+  $.get(url).done(function(response) {
+    function buildRow(index, text) {
+      return `<tr><td>${index}: ${text}</td></tr>`;
+    }
+
+    const rows = [];
+    response
+      .map((item, index) => buildRow(index + 1, item.text))
+      .forEach(row => rows.push(row));
+    
+    $('#id_list_table').html(rows)
+  });
 };
